c306ab80f8774e0d44328e831438bcdddf2a3d26
kazim1101 <kazimcc01@hotmail.com>
2020-06-29 17:51:12 +0400

    done

---
+++ src/Components/sample-component.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Employes extends React.Component {
+
+    render(){
+        return(
+            <div>
+                <h1>Kazim Ali</h1>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Employes/>,document.getElementById('root'))
